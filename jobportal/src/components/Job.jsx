3a66ca30cece60d7dda0ae300582bacabfece4f4
Bappa Biswas <bappabiswas@Bappas-MacBook-Air.local>
2025-08-30 23:05:59 +0530

    add jobpro.jsx and apply.jsx

--- jobportal/src/components/Job.jsx
+++ jobportal/src/components/Job.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+// import ApplyForm from "./ApplyForm"
 
 const Job = () =>{
     return (
         <>
             <Navbar/>
             <h1>This is Job Posting Section</h1>
-            
         </>
     )
 }
