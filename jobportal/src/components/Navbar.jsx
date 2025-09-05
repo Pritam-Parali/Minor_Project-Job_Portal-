b64a2a577ab1129413e4d5ea27566d6b3091079d
Pritam-Parali <pritam2003pp@gmail.com>
2025-09-05 15:30:46 +0530

    updated the navbar with advance effects

--- jobportal/src/components/Navbar.jsx
+++ jobportal/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import search from "../assets/search-w.png"; 
 import jobnest from '../assets/jobnest.png';
+import './Navbar.css'; 
 
 const Navbar = () => {
   return (
@@ -16,15 +17,15 @@ const Navbar = () => {
             </li>
 
             <li><NavLink to="/" className="nav">Home</NavLink></li>
-            <li><NavLink to="/About" className="nav">About Us</NavLink></li>
+            <li><NavLink to="/About" className="nav">About</NavLink></li>
             <li><NavLink to="/Job" className="nav">Job</NavLink></li>
-            <li><NavLink to="/Contactus" className="nav">Contact Us</NavLink></li>
+            <li><NavLink to="/Contactus" className="nav">Contact</NavLink></li>
             
             <li className="search-box">
               <input type="text" placeholder="search" />
               <img src={search} alt="Search" className="logo" />
             </li>
-            <li><NavLink to="/Myprofile" className={"myprofile"}>My Profile</NavLink></li>
+            <li><NavLink to="/Myprofile" className={"myprofile"}>Profile</NavLink></li>
             <li><NavLink to="/Login" className={"login"}>Login</NavLink></li> 
           </ul>
         </nav>
