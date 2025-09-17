370fc586718c75b6dd0306456fa2cfab79ea1a5c
Pritam-Parali <pritam2003pp@gmail.com>
2025-09-17 22:08:14 +0530

    photo added

--- jobportal/src/components/About.jsx
+++ jobportal/src/components/About.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import Navbar from "./Navbar";
 import "./About.css";
 import together from "../assets/together.jpeg";
+import bappa from "../assets/personal_image/bappa.jpg";
+import sayan from "../assets/personal_image/sayan.jpg";
+import arpita from "../assets/personal_image/arpita.jpg";
+import pritam from "../assets/personal_image/pritam.jpg";
+import ali from "../assets/personal_image/ali_sir.jpg";
 
 const About = () => {
   return (
@@ -58,13 +63,14 @@ const About = () => {
             </div>
 
             <div className="textpart">
-              <h3 className="textpart_heading">Our Mission</h3><br/>
-              <p className="textpart_paragraph">To connect talented individuals with their dream careers while helping companies find the perfect candidates.</p>
+              <h3 className="textpart_heading">Our Mission</h3>
+              <br />
+              <p className="textpart_paragraph">
+                To connect talented individuals with their dream careers while
+                helping companies find the perfect candidates.
+              </p>
             </div>
           </div>
-
-
-
 
           <div className="innerdiv1">
             <div className="innerinnerdiv">
@@ -72,14 +78,13 @@ const About = () => {
             </div>
 
             <div className="textpart">
-              <h3 className="textpart_heading">Our Mission</h3><br/>
-              <p className="textpart_paragraph">To connect talented individuals with their dream careers while helping companies find the perfect candidates.</p>
+              <h3 className="textpart_heading">Community First</h3>
+              <br />
+              <p className="textpart_paragraph">
+                We believe in building a supportive community where job seekers and employers can thrive together
+              </p>
             </div>
           </div>
-
-
-
-
 
           <div className="innerdiv1">
             <div className="innerinnerdiv">
@@ -87,12 +92,13 @@ const About = () => {
             </div>
 
             <div className="textpart">
-              <h3 className="textpart_heading">Our Mission</h3><br/>
-              <p className="textpart_paragraph">To connect talented individuals with their dream careers while helping companies find the perfect candidates.</p>
+              <h3 className="textpart_heading">High Quality</h3>
+              <br />
+              <p className="textpart_paragraph">
+                We strive for excellence in everything we do, from our platform features to customer service
+              </p>
             </div>
           </div>
-
-
 
           <div className="innerdiv1">
             <div className="innerinnerdiv">
@@ -100,21 +106,51 @@ const About = () => {
             </div>
 
             <div className="textpart">
-              <h3 className="textpart_heading">Our Mission</h3><br/>
-              <p className="textpart_paragraph">To connect talented individuals with their dream careers while helping companies find the perfect candidates.</p>
+              <h3 className="textpart_heading">Global Reach</h3>
+              <br />
+              <p className="textpart_paragraph">
+                Connecting opportunities across the globe, making the world a smaller place for career growth. 
+              </p>
             </div>
           </div>
         </div>
       </section>
 
+      {/* SECTION 4 STARTING  */}
+
       <section className="section_4">
-          <h3 className="heading_4">Meet Our Team</h3>
-          <p className="paragraph_4">The passionate people behind JobPortal</p>
+        <h3 className="heading_4">Meet Our Team</h3>
+        <p className="paragraph_4">The passionate people behind JobPortal</p>
+        <div className="div4">
+          <div className="innerdiv4">
+            <img src={bappa} alt="bappa"></img>
+            <h3 className="h3">BAPPA BISWAS</h3>
+          </div>
 
+          <div className="innerdiv4">
+            <img src={pritam} alt="pritam"></img>
+            <h3 className="h3">PRITAM PARALI</h3>
+          </div>
 
+          <div className="innerdiv4">
+            <img src={sayan} alt="SAYAN"></img>
+            <h3 className="h3">SAYAN BISWAS</h3>
+          </div>
+          
+
+        </div>
+        <div className="div4">
+          <div className="innerdiv4">
+            <img src={arpita} alt="arpita"></img>
+            <h3 className="h3">ARPITA ROY</h3>
+          </div>
+          <div className="innerdiv4">
+            <img src={sayan} alt="SUPRIYA"></img>
+            <h3 className="h3">SUPRIYA MATTABAR</h3>
+          </div>
+        </div>
+        
       </section>
-
-
     </>
   );
 };
