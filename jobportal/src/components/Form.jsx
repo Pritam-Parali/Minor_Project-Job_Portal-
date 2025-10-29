5487c25fff349bf893f35540ef86f016ce7d364a
Pritam-Parali <pritam2003pp@gmail.com>
2025-10-29 16:53:54 +0530

    added create job page

---
+++ jobportal/src/components/Form.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+import Navbar from "./Navbar";
+import './Form.css';
+
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+function FormExample() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+<>
+    <div>
+        <h6 className="heading">Job posting Details</h6>
+    </div>
+    <div className="form-page">
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="First name*"
+            defaultValue=""
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Last name*</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Last name*"
+            defaultValue=""
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+          <Form.Label>Email id*</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Email id"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please choose a email
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom03">
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" placeholder="City" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom04">
+          <Form.Label>State</Form.Label>
+          <Form.Control type="text" placeholder="State" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid state.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="2" controlId="validationCustom05">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control type="text" placeholder="Zip" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid zip.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      
+    </Form>
+     <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom06">
+          <Form.Label>Domain*</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Type Domain"
+            defaultValue=""
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom07">
+          <Form.Label>Company name*</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Type Company Name here"
+            defaultValue=""
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group as={Col} md="4" controlId="validationCustom07">
+          <Form.Label>Job Location*</Form.Label>
+          <Form.Control
+            
+            type="text"
+            placeholder="Location"
+            defaultValue=""
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        </Row>
+        <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationCustom08">
+          <Form.Label>Salary*</Form.Label>
+          <Form.Control
+            
+            type="number"
+            placeholder="salary"
+            defaultValue=""
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        </Row>
+        <Form.Group className="mb-3">
+        <Form.Check
+          required
+          label="Agree to terms and conditions"
+          feedback="You must agree before submitting."
+          feedbackType="invalid"
+        />
+      </Form.Group>
+      <Button type="submit">Submit form</Button>
+    </div>
+    </>
+  );
+}
+
+export default FormExample;
